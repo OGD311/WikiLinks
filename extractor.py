cf9346136760c19de57a800393a9c2a833a8d9e1
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-05-09 20:49:17 +0100

    Finalised basic extractor, has table for page links and redirects. Contains checks for if a page is a redirect before deciding whether to parse links or not, helping speed up the saving process.

--- extractor.py
+++ extractor.py
@@ -3,6 +3,7 @@ import mwparserfromhell
 from cassandra.cluster import Cluster
 from cassandra.query import BatchStatement, SimpleStatement
 from tqdm import tqdm
+from itertools import islice
 
 # Cassandra setup
 cluster = Cluster(['127.0.0.1'])
@@ -14,29 +15,61 @@ session.execute("""
 """)
 
 session.set_keyspace("wiki")
+session.execute("DROP TABLE IF EXISTS page_links;")
+session.execute("DROP TABLE IF EXISTS redirects;")
 
 # Initialise Cassandra tables
 session.execute("""
     CREATE TABLE page_links (
         title text PRIMARY KEY,
-        links text<set>,
-        is_redirect bool
+        links set<text>,
+        is_redirect boolean
     );   
 """)
 
 session.execute("""
     CREATE TABLE redirects (
-        original text PRIMARY KEY
+        original text PRIMARY KEY,
         redirect text
     );
 """)
 
+insert_page_query = "INSERT INTO page_links (title, links, is_redirect) VALUES (%s, %s, %s)"
 
+insert_redirect_query = "INSERT INTO redirects (original, redirect) VALUES (%s, %s)"
 
 
 # Load wiki dump
-file = "" # Replace with path to wikidump.xml
+file = "enwiki-latest-pages-articles-multistream.xml"
 dump = mwxml.Dump.from_file(open(file, "rb"))
 
 page_count = 6_990_400 # Approximate number of wikipedia 'pages' in the dump
 pbar = tqdm(total=page_count)
+i = 0
+for page in dump:
+   
+
+    for revision in page:
+
+        title = page.title
+
+        if page.redirect:
+            session.execute(insert_page_query, (title, set(), True))
+            session.execute(insert_redirect_query, (title, page.redirect))
+            break
+
+        wikicode = mwparserfromhell.parse(revision.text)
+        links = set(
+                str(link.title).strip()
+                for link in wikicode.filter_wikilinks()
+                if link.title and not link.title.startswith((
+                    'File:', 'Image:', 'Special:', 
+                    'Category:', 'Help:', 'Portal:', 
+                    'Template:', 'Wikipedia:'))
+                )
+        
+        session.execute(insert_page_query, (title, links, False))
+
+        break # Only want the first revision
+
+    pbar.update(1)
