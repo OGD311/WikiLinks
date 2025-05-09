3e63d2fe706fa324cab8c8422740177d077572a0
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-05-10 00:57:26 +0100

    Added Iterative Deepening Depth First Search algorithm, using two queues to manage a seatch to a certain depth, and then a new search starting at that depth, allowing for the route to be found quickly and efficiently.

--- explorer_iddfs.py
+++ explorer_iddfs.py
@@ -3,42 +3,53 @@ from node import Node
 from function_timer import timer
 
 @timer
-def IDDFS(start_title, goal_title, depth=5):
-    max_depth = depth
+def IDDFS(start_title, goal_title, depth=2):
+    queue = searchQueue()
+    nextQueue = searchQueue()
+    visited = set()
 
-    while True:
-        queue = searchQueue()
-        visited = set()
-        queue.enqueue([start_title])
+    queue.enqueue([start_title])
 
-        while not queue.is_empty():
-            current_path = queue.dequeue()
-            current_title = current_path[-1]
+    while not queue.is_empty():
+        current_path = queue.dequeue()
+        current_title = current_path[-1]
+        print(f"Current path: {current_path}, Current title: {current_title}")
 
-            if len(current_path) > max_depth:
-                continue
+        if len(current_path) == depth:
+            nextQueue.enqueue(current_title)
 
-            if current_title in visited:
-                continue
-            visited.add(current_title)
+        if len(current_path) > depth:
+            continue
 
-            try:
-                current_node = Node(current_title)
-            except SyntaxError as e:
-                print(e)
-                continue
+        if queue.is_empty() and len(current_path) <= depth:
+            print(nextQueue.peek())
+            depth *= 2 
+            while not nextQueue.is_empty():
+                title = nextQueue.dequeue()
+                queue.enqueue([title])
 
-            if current_node.node_in_links(goal_title):
-                return current_path + [goal_title]
+        if current_title in visited:
+            continue
+        visited.add(current_title)
 
-            for link in current_node.get_links():
-                if link not in visited:
-                    new_path = current_path + [link]
-                    queue.enqueue(new_path)
+        try:
+            current_node = Node(current_title)
+        except SyntaxError as e:
+            print(e)
+            continue
 
-        max_depth *= 2
+        if current_node.node_in_links(goal_title):
+            return current_path + [goal_title]
+
+        for link in current_node.get_links():
+            if link not in visited:
+                new_path = current_path + [link]
+                queue.enqueue(new_path)
+
+        
 
     return None
+
 
 if __name__ == "__main__":
     path = IDDFS("Amsterdam", "Asia")
@@ -46,3 +57,4 @@ if __name__ == "__main__":
         print("Path found:", " -> ".join(path))
     else:
         print("No path found.")
+
