cffa7af50e5ad0eb598f4d783397fe5353b8256a
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-05-09 22:30:31 +0100

    Basic implementation of a queue in python, with added replace method to allow changing out old values for new ones without loss of queue order.

---
+++ searchQueue.py
@@ -0,0 +1,39 @@
+class searchQueue:
+    def __init__(self):
+        self.items = []
+
+    def get_length(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def show(self):
+        print(self.items)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            return None
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            return None
+        
+
+    def replace(self, currentItem, replaceItem):
+        if not self.is_empty():
+            try:
+                index = self.items.index(currentItem)
+                self.items.pop(index)
+                self.items.insert(index, replaceItem)
+            except:
+                print("Item not in list")
+        else:
+            print("No items to replace")
